d9cd90b0a3346d578c14ef9a4bf6b340e38156c1
lyyyuna <lyyyuna@gmail.com>
2015-06-03 14:55:17 +0800

    identifiability

---
+++ identifiability/Delete_Files.bat
@@ -0,0 +1,16 @@
+erase *.asv
+erase *.aux
+erase *.bak
+erase *.dvi
+erase *.log
+erase *.prn
+erase *.spl
+erase *.toc
+erase h*.tex
+erase h*.ps
+erase y*.ps
+erase y*.spl
+erase ctextemp_*.*
+
+erase 2-*.tex
+erase 2-*.ps
